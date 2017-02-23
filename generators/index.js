11a1e9d4ce0e72903196d79b727865f68ed41eb7
Willwin Wang <willwin.wang@rea-group.com>
2017-02-23 16:54:13 +1100

    Updated generators

--- generators/index.js
+++ generators/index.js
@@ -1,8 +1,12 @@
 function *startProcess() {
   let result = yield;
   console.log(`result is ${result}`);
+  return result;
 }
 
 let iterate = startProcess();
-iterate.next();
-iterate.next(200);
+let foo = iterate.next();
+let bar = iterate.next(200);
+
+console.log('foo', foo);
+console.log('bar', bar);
