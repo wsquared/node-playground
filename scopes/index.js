8e710dc22d32280eb31baee0ea3441b8cc6b70d9
Willwin Wang <willwin.wang@rea-group.com>
2017-02-23 14:40:16 +1100

    Added scopes example

---
+++ scopes/index.js
@@ -0,0 +1,13 @@
+if(true) {
+  var foo = 'foo';
+  console.log('foo1', foo);
+}
+
+console.log('foo2', foo);
+
+if(true) {
+  let bar = 'bar';
+  console.log('bar1', bar);
+}
+
+console.log('bar2', bar);
