a0a885a972a556007397d2dd125b3dc0cd7e3b71
Willwin Wang <willwin.wang@rea-group.com>
2017-08-13 23:39:41 +1000

    Update scopes

--- scopes/index.js
+++ scopes/index.js
@@ -1,13 +1,13 @@
 if(true) {
   var foo = 'foo';
-  console.log('foo1', foo);
+  console.log('foo1', foo); // foo
 }
 
-console.log('foo2', foo);
+console.log('foo2', foo); // foo
 
 if(true) {
   let bar = 'bar';
-  console.log('bar1', bar);
+  console.log('bar1', bar); // bar
 }
 
-console.log('bar2', bar);
+console.log('bar2', bar); // undefined
