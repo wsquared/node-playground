5ce82e6ec5d72e188c871951993ab84cdb3342fe
Willwin Wang <willwin.wang@rea-group.com>
2017-08-13 23:39:15 +1000

    Update

--- iterators/index.js
+++ iterators/index.js
@@ -1,8 +1,9 @@
-let ids = [9000, 9001, 9002];
 let it = ids[Symbol.iterator]();
 
-console.log(it.next());
+console.log('typeof ids: ', typeof ids[Symbol.iterator]);
+
+console.log('it.next(): ', it.next());
 
 for (let v of ids) {
-  console.log(v);
+  console.log('v: ', v);
 }
