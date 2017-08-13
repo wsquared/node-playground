80e1d7720017c7a1aecdaaae7e4683652a367f4f
Willwin Wang <willwin.wang@rea-group.com>
2017-08-14 00:16:08 +1000

    Update weakmaps

--- weakmaps/index.js
+++ weakmaps/index.js
@@ -1,4 +1,24 @@
 let wm1 = new WeakMap();
 let wm2 = new WeakMap();
 
-wm1.set(true, false);
+class MyWeakMap {
+  constructor(v1, v2) {
+    // Keys of WeakMaps are of the type Object only.
+    // Primitive data types as keys are not allowed (e.g. a Symbol can't be a WeakMap key).
+    wm1.set(this, v1);
+    wm2.set(this, v2);
+  }
+
+  getV1() {
+    return wm1.get(this);
+  }
+
+  getV2() {
+    return wm2.get(this);
+  }
+}
+
+const myWeakMap = new MyWeakMap(1, 2);
+
+console.log('myWeakMap v1: ', myWeakMap.getV1());
+console.log('myWeakMap v2: ', myWeakMap.getV2());
